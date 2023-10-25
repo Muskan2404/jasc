3899fd7c11337bdb8472ad80ee08651883ea779e
Muskan2404 <144544617+Muskan2404@users.noreply.github.com>
2023-10-25 18:40:54 +0000

    Add variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 241004
+let accountEmail = "muskan24@outlook.in"
+var accountPassword = "12345"
+accountCity = "Asansol"
+let accountState;
+
+// accountId = 4 // not allowed
+
+accountEmail = "mk04@firefox.in"
+accountPassword = "67890"
+accountCity = "Burnpur"
+
+console.table ([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+
+
+console.log(accountId);
+
+
+/*
+PREFER NOT TO USE VAR! 
+because it cannot be used in case of block and functional scopes({}).
+*/
