f63422842f5e88a3de1d0f7ee928aeb96f16a226
Muskan2404 <144544617+Muskan2404@users.noreply.github.com>
2023-10-25 18:41:13 +0000

    Add datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version. 
+
+/*alert ("hello")  // we are using nodejs , not browser. Hence, alert has a different syntax.
+ alert ( 2+2)
+ */ 
+
+ console.log( 3 
+    + 3);                   // do not write code like this. make it readable 
+  console.log ("Muskan")
+
+//official documentation for js:tc39.es 
+//prettier documentation: mdn
+
+let name = ("Muskan")
+let age = 19
+let isloggein = false
+let State;
+  
+
+//PRIMITIVE DATA TYPES:
+
+//number => 2^2004
+//bigint
+//string => "" (can be written in single quotes but preferably write in double quotes.)
+//boolean => true or false
+//null => standalone value
+//undefined =>
+//symbol => for uniqueness
+
+//object
+
+console.log(typeof undefined); //undefined is a type
+console.log(typeof null); //null is an object
+
