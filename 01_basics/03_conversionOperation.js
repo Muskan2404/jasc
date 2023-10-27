00819e5f0874acccc75b2ee68066cf9b004c1988
Muskan2404 <144544617+Muskan2404@users.noreply.github.com>
2023-10-27 07:10:54 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" => 33
+//"33mks" => NaN(Not a Number)
+//true => 1; false => 0. 
+
+let isLoggedIn = 0
+
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+// console.log (BooleanisLoggedIn)
+// 1 => true; 0 => false
+// "" => false; "muskan" => true
+
+
+let anynumber = 2
+
+let stringNumber = String(anynumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
